2ba7ccb97b58c95c58a6d52732318a33fdb471ae
Ueslei Lima <ueslei@backbase.com>
2021-11-03 18:11:58 +0100

    Configuring proxy configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -56,7 +56,7 @@ file_env 'KEYCLOAK_ADMIN_PASSWORD'
 # Start Keycloak #
 ##################
 
-CONFIG_ARGS="--proxy=edge --http-port=$PORT"
+CONFIG_ARGS="--http.proxy-address-forwarding=true --http-enabled=true --http-port=$PORT"
 RUN_CONFIG_START=false
 RUN_CONFIG=false
 SERVER_OPTS=""
