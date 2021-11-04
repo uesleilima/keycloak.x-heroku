a38351ba63dd22912d46e4a17bc532b7b64989e6
Ueslei Lima <ueslei@backbase.com>
2021-11-04 12:20:03 +0100

    Tidy up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -49,11 +49,11 @@ if [ "$DATABASE_URL" != "" ]; then
 
     regex='^postgres://([a-zA-Z0-9_-]+):([a-zA-Z0-9]+)@([a-z0-9.-]+):([[:digit:]]+)/([a-zA-Z0-9_-]+)$'
     if [[ $DATABASE_URL =~ $regex ]]; then
-        export DB_ADDR=${BASH_REMATCH[3]}
-        export DB_PORT=${BASH_REMATCH[4]}
-        export DB_DATABASE=${BASH_REMATCH[5]}
-        export DB_USER=${BASH_REMATCH[1]}
-        export DB_PASSWORD=${BASH_REMATCH[2]}
+        DB_ADDR=${BASH_REMATCH[3]}
+        DB_PORT=${BASH_REMATCH[4]}
+        DB_DATABASE=${BASH_REMATCH[5]}
+        DB_USER=${BASH_REMATCH[1]}
+        DB_PASSWORD=${BASH_REMATCH[2]}
 
         echo "DB_ADDR=$DB_ADDR, DB_PORT=$DB_PORT, DB_DATABASE=$DB_DATABASE, DB_USER=$DB_USER, DB_PASSWORD=$DB_PASSWORD"
 
